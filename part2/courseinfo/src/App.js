b7442a35f7938a0e41c621860080c623322ae443
PickyOnion <nicolas.messmer@gmail.com>
2022-05-01 18:47:47 +0200

    exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,41 +1,4 @@
-const Part = (props) => {
-  return (
-    <p>
-      {props.props.name} {props.props.exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  const initalValue = 0;
-  const exerciseArr = props.props.parts.map((part) => part.exercises);
-  const total = exerciseArr.reduce(
-    (previousValue, currentValue) => previousValue + currentValue,
-    initalValue
-  );
-
-  return (
-    <>
-      {props.props.parts.map((part) => (
-        <Part props={part} key={part.id} />
-      ))}
-      <p>
-        <b>total of {total} exercises</b>
-      </p>
-    </>
-  );
-};
-
-const Header = (props) => <h2>{props.props.name}</h2>;
-
-const Course = (course) => {
-  return (
-    <>
-      <Header props={course.course} />
-      <Content props={course.course} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
