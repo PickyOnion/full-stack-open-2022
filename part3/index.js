7609dd27cbfee8faad035cc296e136d15cc14f8e
PickyOnion <nicolas.messmer@gmail.com>
2022-06-05 18:05:58 +0200

    exercise 3.5

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -23,6 +25,12 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+function generateId(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -55,6 +63,26 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "invalid body",
+    });
+  }
+
+  const person = {
+    id: generateId(1, 10000000000),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
