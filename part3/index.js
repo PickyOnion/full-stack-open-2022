aac0793877d082162d2a15e6c421201a364e7c6a
PickyOnion <nicolas.messmer@gmail.com>
2022-06-05 18:36:12 +0200

    exercise 3.6

--- part3/index.js
+++ part3/index.js
@@ -68,9 +68,15 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
+  const existingPerson = persons.find((person) => person.name === body.name);
+
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "invalid body",
+      error: "name or number is missing",
+    });
+  } else if (existingPerson) {
+    return response.status(400).json({
+      error: "name must be unique",
     });
   }
 
