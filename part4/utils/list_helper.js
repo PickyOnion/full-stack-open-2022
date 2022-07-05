a18e7ed5e2bfa544b8d3cb3fa95801d09dfb8874
PickyOnion <nicolas.messmer@gmail.com>
2022-07-05 23:11:31 +0200

    exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,14 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  let totalLikes = 0;
+  blogs.forEach((post) => {
+    totalLikes += post.likes;
+  });
+  return totalLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
