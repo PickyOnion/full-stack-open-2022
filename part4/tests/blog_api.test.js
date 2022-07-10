671627be75034786fe1534e0e893085f6e3f1c46
PickyOnion <nicolas.messmer@gmail.com>
2022-07-10 18:34:43 +0200

    exercise 4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+const Blog = require("../models/blog");
+
+const initialPosts = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let postObject = new Blog(initialPosts[0]);
+  await postObject.save();
+  postObject = new Blog(initialPosts[1]);
+  await postObject.save();
+});
+
+test("posts are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("there are two blog posts", async () => {
+  const response = await api.get("/api/blogs");
+  expect(response.body).toHaveLength(2);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
