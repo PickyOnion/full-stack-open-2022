8045aacf7b085e9fe00947f50b008f4d21ea4a37
PickyOnion <nicolas.messmer@gmail.com>
2022-07-22 22:49:25 +0200

    exercise 4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,28 +2,43 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const user = require("../models/user");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 blogsRouter.post("/", async (request, response) => {
-  const defaultUser = await User.findById("62d875a6b1e613557d95afcc");
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
     url: request.body.url,
     likes: request.body.likes,
-    user: defaultUser.id,
+    user: user.id,
   });
 
   const result = await blog.save();
-  defaultUser.blogs = defaultUser.blogs.concat(result._id); // object instead of ID
-  await defaultUser.save();
+  user.blogs = user.blogs.concat(result._id); // object instead of ID
+  await user.save();
   response.status(201).json({
     blog: result,
-    username: defaultUser.username,
+    username: user.username,
   });
 });
 
