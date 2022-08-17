46dd68380564d855bf804c5c212d770217ee12f5
PickyOnion <nicolas.messmer@gmail.com>
2022-08-17 23:18:26 +0200

    exercise 5.7 WIP

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  // console.log("blog", blog);
+
   return (
-    <div>
-      {blog.title} - {blog.author}
+    <div style={blogStyle}>
+      <div>
+        {blog.title} - {blog.author}
+        <button type="submit" onClick={toggleVisibility}>
+          view
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>{`likes ${blog.likes}`}</p>
+        {/* <p>{user.name}</p> */}
+      </div>
     </div>
   );
 };
