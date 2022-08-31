558af7d84049b4ceaa94821e66e2de44f1340a81
PickyOnion <nicolas.messmer@gmail.com>
2022-09-01 00:06:07 +0200

    exercise 5.13 refactor

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -3,40 +3,48 @@ import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
 
-test("renders title and author, doesn't render url & likes", () => {
-  const blog = {
-    title: "Hello",
-    author: "nico",
-    url: "asdas",
-    likes: 0,
-    user: {
-      username: "MrEgger",
-      name: "Eggy Egg",
-      id: "62d9c414e8cfbdc28814763a",
-    },
-    id: "62f68fba6f044724f348d846",
-  };
+describe("<Blog />", () => {
+  let container;
 
-  const handleDelete = () => {
-    console.log("first");
-  };
+  beforeEach(() => {
+    const blog = {
+      title: "Hello",
+      author: "nico",
+      url: "asdas",
+      likes: 0,
+      user: {
+        username: "MrEgger",
+        name: "Eggy Egg",
+        id: "62d9c414e8cfbdc28814763a",
+      },
+      id: "62f68fba6f044724f348d846",
+    };
 
-  const handleUpvote = () => {
-    console.log("Second");
-  };
+    const handleDelete = () => {
+      console.log("first");
+    };
 
-  const { container } = render(
-    <Blog
-      blog={blog}
-      handleDelete={handleDelete}
-      handleUpvote={handleUpvote}
-      username={"MrEgger"}
-    />
-  );
+    const handleUpvote = () => {
+      console.log("Second");
+    };
 
-  const authorTitle = screen.getByText("Hello - nico");
-  expect(authorTitle).toBeDefined();
+    container = render(
+      <Blog
+        blog={blog}
+        handleDelete={handleDelete}
+        handleUpvote={handleUpvote}
+        username={"MrEgger"}
+      />
+    ).container;
+  });
 
-  const div = container.querySelector(".togglableContent");
-  expect(div).toHaveStyle("display: none");
+  test("renders title and author", async () => {
+    const authorTitle = await screen.getByText("Hello - nico");
+    expect(authorTitle).toBeDefined();
+  });
+
+  test("url & likes are hidden", () => {
+    const div = container.querySelector(".togglableContent");
+    expect(div).toHaveStyle("display: none");
+  });
 });
