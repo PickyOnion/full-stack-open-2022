64394e5beb4090276c03c3a2b5dc5b05c3bc3013
PickyOnion <nicolas.messmer@gmail.com>
2022-09-15 22:55:26 +0200

    exercise 6.4

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -7,13 +7,24 @@ const anecdotesAtStart = [
   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
 ];
 
-const getId = () => (100000 * Math.random()).toFixed(0);
+export const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
     votes: 0,
+  };
+};
+
+export const createAnecdote = (content) => {
+  return {
+    type: "NEW_ANECDOTE",
+    data: {
+      content,
+      id: getId(),
+      votes: 0,
+    },
   };
 };
 
@@ -46,6 +57,10 @@ const reducer = (state = initialState, action) => {
       });
 
       return newState;
+
+    case "NEW_ANECDOTE":
+      return state.concat(action.data);
+
     default:
       return state;
   }
