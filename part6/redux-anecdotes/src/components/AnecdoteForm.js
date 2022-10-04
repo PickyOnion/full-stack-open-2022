ca21c5aeef60ddafd232af04316cfa98e784130f
PickyOnion <nicolas.messmer@gmail.com>
2022-10-04 21:23:25 +0200

    exercise 6.20

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,13 +1,11 @@
-import { useDispatch } from "react-redux";
+import { connect } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
-const AnecdoteForm = () => {
-  const dispatch = useDispatch();
-
+const AnecdoteForm = (props) => {
   const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
-    dispatch(createAnecdote(content));
+    props.createAnecdote(content);
   };
 
   return (
@@ -23,4 +21,6 @@ const AnecdoteForm = () => {
   );
 };
 
-export default AnecdoteForm;
+const ConnectedAnecdoteForm = connect(null, { createAnecdote })(AnecdoteForm);
+
+export default ConnectedAnecdoteForm;
